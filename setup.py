6c3fb0be931f86cd1fee407a8e2bfd8ac330113f
yilmazfirat <yilmazfsalman@gmail.com>
2024-07-14 10:27:20 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'mcqgen', 
+    version = '0.0.1',
+    author ='ysalman',
+    author_email = 'ylzami@gmail.com',
+    packages = find_packages()
+)
